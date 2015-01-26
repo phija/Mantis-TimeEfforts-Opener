1d92156f619b7f654ed77f965301237f45ddb47d
phija <github@phiworld.de>
2015-01-26 16:36:00 +0100

    fixed version tag

--- Mantis_TimeEfforts_Opener.user.js
+++ Mantis_TimeEfforts_Opener.user.js
@@ -6,7 +6,7 @@
 // @include     http://mantis-URL/plugin.php?page=TimeEffortAnalysis/developerView
 // @include     http://mantis-URL/plugin.php?page=TimeEffortAnalysis/productView
 // @grant       none
-// version      0.1.0
+// @version      0.1.1
 // ==/UserScript==
 
 var developerToOpen = "";
